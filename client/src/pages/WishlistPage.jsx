0314c79bc209a77b358d0c8d92ffb0455e189d31
Shukrullo <abdusalomovjovox@gmail.com>
2026-09-12 19:54:38 +0500

    sayt edit

--- client/src/pages/WishlistPage.jsx
+++ client/src/pages/WishlistPage.jsx
@@ -37,11 +37,11 @@ const WishlistPage = () => {
     return (
       <div className="wishlist-page wishlist-page--empty">
         <div className="wishlist-empty">
-          <img
-            src="/img/wishlist_preview_rev_1.png"
-            alt={t('wishlist.emptyText')}
-            className="wishlist-empty-img"
-          />
+          <div className="wishlist-empty-img" aria-hidden="true">
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+            </svg>
+          </div>
           <p className="wishlist-empty-text">
             {t('wishlist.emptyText')}
           </p>
